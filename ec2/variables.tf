f4c397bb94bc0d224904dd6f34c43ed2f1c5c0c1
pshyamala <vskumarcloud41@gmail.com>
2025-07-14 17:04:13 +0530

    Update variables.tf

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -1,1 +1,14 @@
+variable "ami_id" {
+  type        = string
+  description = "AMI ID to use for EC2"
+}
 
+variable "instance_type" {
+  type        = string
+  description = "EC2 instance type"
+}
+
+variable "instance_name" {
+  type        = string
+  description = "Name tag for the EC2 instance"
+}
